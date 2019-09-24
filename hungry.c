072dda5c6894610345ead26ed3304697bb784615
Franky-H <36466992+Franky-H@users.noreply.github.com>
2019-09-24 13:02:09 +0900

    Update hungry.c

--- hungry.c
+++ hungry.c
@@ -5,43 +5,70 @@
 #include <string.h>
 #include <pthread.h>
 #include <semaphore.h>     
-#define N 5
 
+#define NUM_THREAD   5
 
-struct msg{
-	long mtype;
-	int fork1;
-	int fork2;
-	int state[N];
-} gmsg;
+sem_t sem[NUM_THREAD];
 
-
-
-int main()
+void eating(int index)
 {
+	if(index==0)
+	{
+		printf("Philosopher %d is hungry\n",index+1);
+		sem_wait(&sem[index]);
+		sem_wait(&sem[NUM_THREAD-(index+1)]);
+		printf("Philosopher %d takes fork %d and %d\n",index+1,NUM_THREAD-index,index+1 );
+		printf("Philosopher %d is eating...\n");
+		sleep(1);
+		printf("Philosopher %d is putting for %d and %d\n",index+1,NUM_THREAD-index,index+1 );
+		sem_post(&sem[index]);
+		sem_post(&sem[NUM_THREAD-(index+1)]);
+		printf("Philosopher %d is Thinking...\n",index+1);
+		sleep(2);
+	}
+	else
+	{
+		printf("Philosopher %d is hungry\n",index+1);
+		sem_wait(&sem[index-1]);
+		sem_wait(&sem[index]);
+		printf("Philosopher %d takes fork %d and %d\n",index+1,index,index+1);
+		printf("Philosopher %d is eating...\n");
+		sleep(1);
+		printf("Philosopher %d is putting for %d and %d\n",index+1,index, index+1);
+		sem_post(&sem[index-1]);
+		sem_post(&sem[index]);
+		printf("Philosopher %d is Thinking...\n",index+1);
+		sleep(2);
+	}
+}
 
-	pid_t pid[5];
-	key_t key = 0;
-	
-	int i;
-	int mqid;
- 
-	key=ftok("sample_run_3",0);
-	mqid=msgget(key,IPC_CREAT);
- 
+void *hello_thread(void *arg)
+{
+	int index = (int *)arg;
+	while(1)
+	{
+		eating(index);
+	}
+}
 
-	for(i=0;i<5<i++)
-		{
-		pid[i]=fork();
-		if(pid[i]==0)
-			{
-				while(1)
-				{
-				printf("philosopher%d is hungry...",i);
-				}
-			}
+int main(void)
+{
+	pthread_t tid[NUM_THREAD];
+	int status, i;
 
+
+	for(i=0;i<NUM_THREAD;i++)
+		sem_init(&sem[i],0,1);
+
+	for(i=0; i<NUM_THREAD; i++) {
+		status = pthread_create(&tid[i], NULL, hello_thread, (void *)i);
+		if(status != 0) {
+			perror("thread create");
+			exit(1);
 		}
+		
+	}
 
-	
+	pthread_exit(NULL);	
+    return 0;
 }
